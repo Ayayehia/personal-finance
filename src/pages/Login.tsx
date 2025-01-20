5a96a1f0480c1ab3596fac2b81d54e4f6d15cebe
ayayehia <ayanader8@gmail.com>
2025-01-20 20:42:01 +0200

    Refactor Login page layout and integrate Tailwind CSS styles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { LoginSVG } from "../assets/IconsLibrary";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <div className="flex h-screen w-full">
+      <div className="bg-pink-500 w-2/5">40%</div>
+      <div className="bg-blue-300 w-3/5">60%</div>
+    </div>
+  );
 };
 
 export default Login;
