e03bd9dd707b3ea947c82dcb41ea783c654e6f48
ayayehia <ayanader8@gmail.com>
2025-02-01 16:43:38 +0200

    Enhance Login page layout for better responsiveness and update password label

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -67,28 +67,29 @@ const Login = () => {
       </div>
     );
   }
-
   return (
-    <div className="flex w-full min-h-screen">
-      <div className="w-2/5">
-        <section className="bg-dark-grey rounded-xl h-full p-4 flex flex-col">
-          <img src={logo} alt="logo" className="w-fit" />
+    <div className="lg:flex w-full min-h-screen">
+      <div className="lg:w-2/5 hidden lg:block">
+        <section className="bg-dark-grey rounded-xl h-full  flex flex-col">
+          <img src={logo} alt="logo" className="w-fit pt-10 pl-10" />
           <img
             src={DesktopImage}
             alt="sideimage for login page"
             className="h-auto w-full object-cover"
           />
-          <h2 className="font-bold text-4xl text-wrap pb-4">
-            Keep track of your money and save for your future
-          </h2>
-          <p>
-            Personal finance app puts you in control of your spending. Track
-            transactions, set budgets, and add to savings pots easily.
-          </p>
+          <article className="py-10 px-10">
+            <h2 className="font-bold text-4xl text-wrap pb-4">
+              Keep track of your money and save for your future
+            </h2>
+            <p>
+              Personal finance app puts you in control of your spending. Track
+              transactions, set budgets, and add to savings pots easily.
+            </p>
+          </article>
         </section>
       </div>
-      <div className="w-3/5">
-        <div className="flex items-center justify-center h-full w-full">
+      <div className="lg:w-3/5 w-full">
+        <div className="flex items-center justify-center h-full w-full ">
           <Card className="flex flex-col items-start p-8 space-y-8 max-w-md w-full">
             <div className="w-full">
               <Auth
@@ -103,7 +104,7 @@ const Login = () => {
                     },
                     sign_up: {
                       email_label: "Email",
-                      password_label: "password",
+                      password_label: "Create password",
                       button_label: "Create Account",
                       link_text: "Need to create an account? Sign up",
                     },
